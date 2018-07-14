c1c7d10fcb97fee250e76a5756ef174ae7515e0d
Daniel Ruthardt <daniel@dionix.at>
2018-07-14 08:52:12 +0200

    Re-factored Docker image as multi-stage image, thus reducing final image size.

--- tools/graalvm/entrypoint.sh
+++ tools/graalvm/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # add GraalVM's bin directories to the path
-export PATH=$PATH:/opt/graalvm-ce-$GRAALVM_VERSION/bin:/opt/graalvm-ce-$GRAALVM_VERSION/jre/bin
+export PATH=$PATH:/opt/graalvm-ce/bin:/opt/graalvm-ce/jre/bin
 
 # execute the given command
 $@
