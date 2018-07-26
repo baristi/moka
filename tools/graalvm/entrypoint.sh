63bb7c08fbba04f41e665bca33b5fd7c64dd210d
Daniel Ruthardt <dr@zoesolutions.eu>
2018-07-26 22:30:40 +0200

    Execute commands in the context directory.

--- tools/graalvm/entrypoint.sh
+++ tools/graalvm/entrypoint.sh
@@ -3,5 +3,5 @@
 # add GraalVM's bin directories to the path
 export PATH=$PATH:/opt/graalvm-ce/bin:/opt/graalvm-ce/jre/bin
 
-# execute the given command
-$*
+# execute the given command in the context directory
+cd /context && $*
