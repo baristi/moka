0f13d08ae9af5b208c0c8dd14cfc6a8cf04f594b
Daniel Ruthardt <daniel@dionix.at>
2018-08-02 21:58:19 +0200

    Fixed previous commit, the wrong lines were comitted.

--- src/js/nodejs/classes/MokaObject.js
+++ src/js/nodejs/classes/MokaObject.js
@@ -1,6 +1,13 @@
-  // constructor(data) {
-  //   for (let key in data) {
-  //     this[key] = data[key];
-  //   }
-  // }
+const Record = require("js-data").Record;
 
+module.exports = class MokaObject extends Record {
+
+  constructor(data = {}) {
+    super(data);
+  }
+
+  static async getById(id) {
+    return await store.find(this.name, id);
+  }
+
+}
